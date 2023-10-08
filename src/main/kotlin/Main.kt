2d552057fb7b1608006adfcdd03edf66460fd3ff
Serhat <me.serhaterdem@gmail.com>
2023-10-08 17:34:18 +0300

    -

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,37 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.lang.Exception
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+
+    fun findSquare(a:Float):Float{
+        var squareA:Float=0F;
+
+        //define sensitivity
+        val range=a-0.0000000001F
+        while (squareA*squareA!=a){
+            //increase the value until condition will be true
+            squareA+=0.000001F
+            if (squareA*squareA>range) break
+
+        }
+        return squareA
+
+    }
+
+    println("Please enter the number which you want to learn square roots:")
+    var number:String= readln()
+    number+="F"
+    var orgNumber:Float
+    try {
+        orgNumber=number.toFloat()
+    }catch (a:Exception){
+        throw Exception("please enter valid number,ex :1.121")
+        println(number)
+    }
+
+    println( findSquare(orgNumber))
+
+
+
+
+
 }
